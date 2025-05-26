0a1ea3fb6ead469eb9c803c57db8b3d4fe0b0e00
ricefordinr <truongkienthanh6705@gmail.com>
2025-05-26 23:21:38 +0700

    update discord installer

--- discord-installer.sh
+++ discord-installer.sh
@@ -2,10 +2,27 @@
 
 set -e
 
-INSTALL_DIR="$HOME/applications/discord/data"
+INSTALL_DIR="$HOME/applications/discord/Discord"
 DOWNLOAD_DIR="/tmp/discord-download"
 DISCORD_URL="https://discord.com/api/download?platform=linux&format=tar.gz"
 DESKTOP_FILE="$HOME/.local/share/applications/discord.desktop"
+VENCORD_URL="https://github.com/Vendicated/VencordInstaller/releases/latest/download/VencordInstallerCli-Linux"
+VENCORD_DIR="$DOWNLOAD_DIR/vencord"
+
+var=""
+
+for arg in "$@"; do
+    case "$arg" in
+        --var=*)
+            var="${arg#--var=} "
+            ;;
+        *)
+            args+=("$arg")
+            ;;
+    esac
+done
+
+rm -rf "$INSTALL_DIR"
 
 mkdir -p "$INSTALL_DIR"
 mkdir -p "$DOWNLOAD_DIR"
@@ -15,30 +32,29 @@ curl -L "$DISCORD_URL" -o "$DOWNLOAD_DIR/discord.tar.gz"
 
 tar -xf "$DOWNLOAD_DIR/discord.tar.gz" -C "$DOWNLOAD_DIR"
 
-VERSION=$(date +%Y%m%d_%H%M%S)
-mv "$DOWNLOAD_DIR/Discord" "$INSTALL_DIR/Discord-$VERSION"
-
-ln -sfn "$INSTALL_DIR/Discord-$VERSION" "$INSTALL_DIR/current"
+mv "$DOWNLOAD_DIR/Discord" "$INSTALL_DIR/.."
 
 rm -rf "$DESKTOP_FILE"
 if [ ! -f "$DESKTOP_FILE" ]; then
 cat > "$DESKTOP_FILE" <<EOF
 [Desktop Entry]
 Name=Discord (Manual)
-Exec=env DISPLAY=:11 $INSTALL_DIR/current/Discord
-Icon=$INSTALL_DIR/current/discord.png
+Exec=$var$INSTALL_DIR/Discord
+Icon=$INSTALL_DIR/discord.png
 Type=Application
 Categories=Network;Chat;
 EOF
     echo "Recreated $DESKTOP_FILE"
 fi
 
-echo "Discord downloaded successfully at: $INSTALL_DIR/current"
+echo "Discord downloaded successfully at: $INSTALL_DIR"
 
 echo "Installing Vencord"
 
-curl -sSLf "https://github.com/Vendicated/VencordInstaller/releases/latest/download/VencordInstallerCli-Linux" -o $DOWNLOAD_DIR/vencord
-chmod a+x $DOWNLOAD_DIR/vencord
-$DOWNLOAD_DIR/vencord -install -location $INSTALL_DIR/current
+curl -sSLf "$VENCORD_URL" -o $VENCORD_DIR
+chmod a+x $VENCORD_DIR
+$VENCORD_DIR -install -location $INSTALL_DIR
+
+rm -rf $DOWNLOAD_DIR
 
 echo "Let's go!!"
